d2350a2165b532c05ef2f8267eb175a22eaf1cba
Hyun Frantz <hyunfrantz@gmail.com>
2016-12-20 07:29:19 -0500

    Explorer mode complete

---
+++ code.js
@@ -0,0 +1,11 @@
+var greeting = 'Hello, World!';
+
+var my_greeting = 'Hey, you!';
+
+var my_number = 123456;
+
+ my_number.toString();
+
+var my_books = ['Poisonwood Bible', 'Cooked', 'Prodigal Summer', 'The Little Prince'];
+
+var jasmine = {name: "Hyun-Jasmine Frantz", gender: "female", language_spoken: "Korean, English, French", ethicity: "Korean"};
